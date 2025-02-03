7acbb0eed62688e82df35173487712a4bc3b6836
andrewkehoe <sulphyr.yt@gmail.com>
2025-02-03 16:43:47 -0500

    Error handler for vlc not found

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import time, pexpect
 print("maestro cli - neomodulo 2025\n")
 
-vlc = pexpect.spawn("vlc -I rc '../../test track.mp3'", encoding='utf-8')
+try:
+    vlc = pexpect.spawn("vlc -I rc '../../test track.mp3'", encoding='utf-8')
+except pexpect.exceptions.ExceptionPexpect:
+    print("Error: Could not find VLC. Is it in your PATH?\n")
+    print("Windows:   Add path to VLC in Control Panel")
+    print("macOS:     Add path to VLC to /etc/paths ")
+    quit()
 
 for i in range(1,3):
     time.sleep(2)
